39bdca1286b51875a421868917cf0b05c4ba1def
Gabriel Dantas <gabrieldantaslp@gmail.com>
2023-03-03 19:31:05 -0300

    Configurando conexão com o banco de dados

---
+++ backend/src/models/tasks.model.js
@@ -0,0 +1,7 @@
+const findAll = () => {
+
+};
+
+module.exports = {
+    findAll
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,6 @@
 const app = require('./app');
+require('dotenv').config();
 
-app.listen(3333, () => console.log('Rodando!'));
+const PORT = process.env.PORT || 3333;
+
+app.listen(3333, () => console.log(`Rodando! Porta: ${PORT}`));
