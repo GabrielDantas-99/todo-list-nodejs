b3204a56bebcf0c27d4aaf85bfd736667131c224
Gabriel Dantas <gabrieldantaslp@gmail.com>
2023-03-04 12:58:36 -0300

    Middleware para validacao de taskCreate

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const tasksController = require('./controllers/tasks.controller');
+
 const router = express.Router();
 
+const tasksController = require('./controllers/tasks.controller');
+const tasksMiddleware = require('./middlewares/tasks.middleware');
+
 router.get('/tasks', tasksController.findAll);
-router.post('/tasks', tasksController.taskCreate);
+router.post('/tasks',tasksMiddleware.validateBody, tasksController.taskCreate);
 
 module.exports = router;
